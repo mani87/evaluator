d9bb615721105684e28bb5e24c1f554763ed7587
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2017-09-15 08:41:34 +0530

    evaluate.html: Fix color bug

--- evaluator/templatetags/evaluator_filters.py
+++ evaluator/templatetags/evaluator_filters.py
@@ -1,7 +1,12 @@
 from django import template
+import random
 
 register = template.Library()
 
 @register.filter
 def get_question(value, number):
 	return value[number-1].statement
+
+@register.filter
+def get_color(value):
+	return random.choice(["#FFCA28", "#2196F3", "#ASAAAE", "#6DD8D1F", "#9CCC65", "#EC407A", "#666666"])
